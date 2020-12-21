34b06b54fc0ff78d32e0f614b4cde5a818fd1058
gitlng <61177105+gitlng@users.noreply.github.com>
2020-12-21 22:24:51 +0300

    Now works.

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.get('/player.js', function(request, response) {
 
 var who_connected = [];
 
+var steps = 0;
+
 var field = [
     [],
     [],
@@ -41,9 +43,6 @@ for (let i = 0; i < 10; i++) {
     }
 }
 
-field[2][1] = 1;
-
-console.log(who_connected);
 
 function isFirstStep(player_id) {
     console.log('can_step_here');
@@ -52,7 +51,8 @@ function isFirstStep(player_id) {
         for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
                 sum += field[i][j];
-                if (!sum) {
+                if (sum !== 0) {
+                    console.log('SUM =' + sum);
                     console.log('NOT FIRST STEP');
                     return false;
                 }
@@ -61,11 +61,13 @@ function isFirstStep(player_id) {
         console.log('FIRST step');
         return sum === 0;
     } else {
+        sum = 0;
         if (player_id === 2) {
             for (let i = 0; i < 10; i++) {
                 for (let j = 0; j < 10; j++) {
                     sum += field[i][j];
-                    if (sum > 1) {
+                    if (sum > 5) {
+                        console.log('SUM =' + sum);
                         console.log('NOT FIRST STEP');
                         return false;
                     }
@@ -73,14 +75,14 @@ function isFirstStep(player_id) {
             }
         }
         console.log('FIRST step');
-        return sum === 1;
+        return true;
     }
 }
 
 function notAlone(x, y, player_id) {
     console.log('notAlone()');
-    for (let i = 0; i < 2; i++) {
-        for (let j = -1; j < 1; j++ ) {
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++ ) {
             if (player_id === 1) {
                 console.log('Player 1 ', x+i, y+j, '= ', field[x+i][y+j]);
                 if (field[x+i][y+j] === 1) {
@@ -88,8 +90,8 @@ function notAlone(x, y, player_id) {
                     return true;
                 } else if (field[x+i][y+j] === 3) {
                     console.log('[UPPER] Not found friend at ', x+i, y+j);
-                    for (let k = -1; k < 1; k++) {
-                        for (let l = -1; l < 1; l++) {
+                    for (let k = -1; k < 2; k++) {
+                        for (let l = -1; l < 2; l++) {
                             if (field[x+i+k][y+j+l] === 1) {
                                 console.log('[LOWER] Found friend at ', x+i+k, y+j+l);
                                 return true;
@@ -101,15 +103,19 @@ function notAlone(x, y, player_id) {
                     }
                 }
             } else {
-                console.log('Player 2 ', x+i, y+j);
+                console.log('Player 2 ', x+i, y+j, '= ', field[x+i][y+j]);
                 if (field[x+i][y+j] === 2) {
+                    console.log('[UPPER] Found friend at ', x+i, y+j);
                     return true;
                 } else if (field[x+i][y+j] === 4) {
+                    console.log('[UPPER] Not found friend at ', x+i, y+j);
                     for (let k = -1; k < 1; k++) {
                         for (let l = -1; l < 1; l++) {
                             if (field[x+i+k][y+j+l] === 2) {
+                                console.log('[LOWER] Found friend at ', x+i+k, y+j+l);
                                 return true;
                             } else {
+                                console.log('[LOWER] Not Found friend at ', x+i+k, y+j+l);
                                 return false;
                             }
                         }
@@ -121,25 +127,26 @@ function notAlone(x, y, player_id) {
     return false;
 }
 
+
 function canStepHere(x, y, player_id) {
-    if (isFirstStep(player_id) || notAlone(x, y, player_id)) {
-        if (player_id === 1) {
-            field[x][y] = 1;
-        } else {
-            field[x][y] = 2;
-        }
-        return true;
-    } else {
+    if (field[x][y] === 3 || field[x][y] === 4){
+        console.log('CANT STEP HERE! OVERRIDES!');
         return false;
     }
+    if (isFirstStep(player_id)) {
+        return true;
+    }
+    else {
+        return notAlone(x, y, player_id);
+    }
 }
+
+let steps_1st = 3;
+let steps_2nd = 0;
 
 io.sockets.on('connection', function(socket) {
     console.log('A player has been connected.');
     who_connected.push(socket);
-
-    io.sockets.emit('how_many_connected', who_connected.length)
-
     socket.on('disconnect', function(data) {
         console.log('A player has been disconnected.');
         who_connected.splice(who_connected.indexOf(socket), 1);
@@ -147,11 +154,62 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('cell_click', function (data) {
         console.log(data);
-        // console.log(canStepHere(data[0], data[1], data[2]));
         let isStep = canStepHere(data[0], data[1], data[2]);
         console.log(isStep);
-        io.sockets.emit('step',1);
-        console.log('AFTER');
+        if (isStep) {
+            if (data[2] === 1) {
+                    if (steps_1st > 0) {
+                        console.log('FIRST PLAYER : ' + steps_1st + ' STEP');
+                        if (field[data[0]][data[1]] === 2) {
+                            io.sockets.emit('step', {
+                                step: isStep,
+                                player: data[2],
+                                x: data[0],
+                                y: data[1],
+                                attack: true
+                            });
+                            field[data[0]][data[1]] = 3;
+                        } else {
+                            io.sockets.emit('step', {
+                                step: isStep,
+                                player: data[2],
+                                x: data[0],
+                                y: data[1],
+                                attack: false
+                            });
+                            field[data[0]][data[1]] = 1;
+                        }
+                        steps_2nd++;
+                        steps_1st--;
+                    }
+            } else {
+                if (steps_2nd > 0) {
+                    console.log('SECOND PLAYER : ' + steps_2nd + ' STEP');
+                    if (data[2] === 2 && field[data[0]][data[1]] === 1) {
+                        io.sockets.emit('step', {
+                            step: isStep,
+                            player: data[2],
+                            x: data[0],
+                            y: data[1],
+                            attack: true
+                        });
+                        field[data[0]][data[1]] = 4;
+                    } else {
+                        io.sockets.emit('step', {
+                            step: isStep,
+                            player: data[2],
+                            x: data[0],
+                            y: data[1],
+                            attack: false
+                        });
+                        field[data[0]][data[1]] = 2;
+                    }
+
+                    steps_1st++;
+                    steps_2nd--;
+                }
+            }
+        }
     });
 
 });
--- player.js
+++ player.js
@@ -85,13 +85,6 @@ document.querySelector('#cell_28').onclick = function () {
 
 
 
-
-
-document.querySelector('#cell_30').onclick = function () {
-    let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [3, 0, parseInt(player_id)]);
-};
-
 document.querySelector('#cell_31').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
     socket.emit('cell_click', [3,1, parseInt(player_id)]);
@@ -127,13 +120,13 @@ document.querySelector('#cell_37').onclick = function () {
     socket.emit('cell_click', [3,7, parseInt(player_id)]);
 };
 
-
-
-
-document.querySelector('#cell_40').onclick = function () {
+document.querySelector('#cell_38').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [4, 0, parseInt(player_id)]);
+    socket.emit('cell_click', [3,8, parseInt(player_id)]);
 };
+
+
+
 
 document.querySelector('#cell_41').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
@@ -170,13 +163,13 @@ document.querySelector('#cell_47').onclick = function () {
     socket.emit('cell_click', [4,7, parseInt(player_id)]);
 };
 
-
-
-
-document.querySelector('#cell_50').onclick = function () {
+document.querySelector('#cell_48').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [5, 0, parseInt(player_id)]);
+    socket.emit('cell_click', [4,8, parseInt(player_id)]);
 };
+
+
+
 
 document.querySelector('#cell_51').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
@@ -213,60 +206,159 @@ document.querySelector('#cell_57').onclick = function () {
     socket.emit('cell_click', [5,7, parseInt(player_id)]);
 };
 
-
-document.querySelector('#cell_60').onclick = function () {
+document.querySelector('#cell_58').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2, 0, parseInt(player_id)]);
+    socket.emit('cell_click', [5,8, parseInt(player_id)]);
 };
+
+
 
 document.querySelector('#cell_61').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,1, parseInt(player_id)]);
+    socket.emit('cell_click', [6,1, parseInt(player_id)]);
 };
 
 document.querySelector('#cell_62').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,2, parseInt(player_id)]);
+    socket.emit('cell_click', [6,2, parseInt(player_id)]);
 };
 
 document.querySelector('#cell_63').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,3, parseInt(player_id)]);
+    socket.emit('cell_click', [6,3, parseInt(player_id)]);
 };
 
 document.querySelector('#cell_64').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,4, parseInt(player_id)]);
+    socket.emit('cell_click', [6,4, parseInt(player_id)]);
 };
 
 document.querySelector('#cell_65').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,5, parseInt(player_id)]);
+    socket.emit('cell_click', [6,5, parseInt(player_id)]);
 };
 
 document.querySelector('#cell_66').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,6, parseInt(player_id)]);
+    socket.emit('cell_click', [6,6, parseInt(player_id)]);
 };
 
 document.querySelector('#cell_67').onclick = function () {
     let player_id = document.querySelector('#player_id').value;
-    socket.emit('cell_click', [2,7, parseInt(player_id)]);
+    socket.emit('cell_click', [6,7, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_68').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [6,8, parseInt(player_id)]);
 };
 
 
 
 
 
+document.querySelector('#cell_71').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,1, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_72').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,2, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_73').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,3, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_74').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,4, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_75').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,5, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_76').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,6, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_77').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,7, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_78').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [7,8, parseInt(player_id)]);
+};
 
 
 
 
+document.querySelector('#cell_81').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,1, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_82').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,2, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_83').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,3, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_84').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,4, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_85').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,5, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_86').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,6, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_87').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,7, parseInt(player_id)]);
+};
+
+document.querySelector('#cell_88').onclick = function () {
+    let player_id = document.querySelector('#player_id').value;
+    socket.emit('cell_click', [8,8, parseInt(player_id)]);
+};
 
 
-socket.on('step', function(canStep) {
-    if (canStep) {
-        document.querySelector('#cell_00').innerHTML = 'X'
+let count_players = 0
+
+socket.on('step', function(data) {
+    if (data.step) {
+        if (data.player === 1) {
+            if (data.attack) {
+                document.querySelector('#cell_' + data.x + data.y).innerHTML = '(X)';
+            } else {
+                document.querySelector('#cell_' + data.x + data.y).innerHTML = 'x';
+            }
+        } else {
+            if (data.attack) {
+                document.querySelector('#cell_' + data.x + data.y).innerHTML = '(O)';
+            } else {
+                document.querySelector('#cell_' + data.x + data.y).innerHTML = 'O';
+            }
+        }
     }
 })
+
 
